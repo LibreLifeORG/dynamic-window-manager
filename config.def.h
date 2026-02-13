01e3702c9e76c7355380248655a89ddee1154cbf
Ambrose <me@librelife.org>
2026-02-12 23:11:50 -0500

    Cleaned up bindings & bound screenshot script

--- config.def.h
+++ config.def.h
@@ -76,11 +76,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *msg[]  = { "signal-desktop", NULL };
-static const char *hide[]  = { "xhide", NULL };
-static const char *show[]  = { "xshow", NULL };
 static const char *audset[]  = { "audset", NULL };
 static const char *wincap[]  = { "wincap", NULL };
-static const char *screencap[]  = { "screencap", NULL };
+static const char *screenshot[]  = { "screenshot", NULL };
 static const char *browser[]  = { "librewolf", NULL };
 static const char *fileman2[]  = { "thunar", NULL };
 static const char *email[]  = { "claws-mail", NULL };
@@ -98,7 +96,7 @@ static const Key keys[] = {
 	{ MODKEY, 	                XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY, 	                XK_b,      spawn,          {.v = browser } },
 	{ MODKEY, 	                XK_F9,     spawn,          {.v = wincap } },
-	{ MODKEY, 	                XK_F10,    spawn,          {.v = screencap } },
+	{ 0,                            XK_Print,  spawn,          {.v = screenshot } },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = fileman2 } },
 	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = email } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = fileman } },
@@ -107,14 +105,10 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      spawn,          {.v = music } },
 	{ ShiftMask, 	                XK_F2,     spawn,          {.v = volup } },
 	{ ShiftMask, 	                XK_F1,     spawn,          {.v = voldown } },
-	{ MODKEY, 	                XK_F3,     spawn,          {.v = hide } },
-	{ MODKEY, 	                XK_F4,     spawn,          {.v = show } },
 	{ MODKEY|ShiftMask, 	        XK_F1,     spawn,          {.v = audset } },
 	{ MODKEY,                       XK_i,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	/*{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },*/
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
@@ -123,7 +117,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	/*{ MODKEY,                       XK_space,  setlayout,      {0} },*/
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
 	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
